7315b84a0e1d76bdd12ab384bf19ad6c8ce57bb7
Kaio-2112 <k.manoel@escolar.ifrn.edu.br>
2024-08-12 16:10:28 -0300

    tarefa de algoritmos

---
+++ paxta de motors/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+
+void Soma(float a, float b){
+    float resultado = a + b;
+    Console.WriteLine($"{a} + {b} = {resultado}");
+}
+
+void Subtraçao(float a, float b){
+    float resultado = a - b;
+    Console.WriteLine($"{a} + {b} = {resultado}");
+}
+
+void Multiplicacao(float a, float b){
+    float resultado = a * b;
+    Console.WriteLine($"{a} + {b} = {resultado}");
+}
+
+void Divisao(float a, float b, object resultado)
+    if(b == 0){
+        Console.WriteLine($"{a} / {b} = {"Erro de divisao por zero"});
+        return;
+    }
+
+
+    float resultado = a / b;
+    Console.WriteLine($"{a} / {b} = {resultado}");
+    }
+
+//variaveis globais
+float a;
+float b;
+
+void DigitarAeB(){
+    Console.WriteLine("Digite a:");
+    a = float.Parse(Console.ReadLine());
+    Console.WriteLine("Digite b:");
+    b = float.Parse(Console.ReadLine());
+}
+
+
+
+
